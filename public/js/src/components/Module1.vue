82d4afa6947c2ceed2d0ddeda6952cf9bc147481
Rem <dkushneriov@aurum-soft.com>
2018-09-05 19:10:37 +0300

    ust installed base vue router

---
+++ public/js/src/components/Module1.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{name}}</h1>
+</template>
+
+<script>
+    export default {
+        name: "Module1",
+        data() {
+            return {
+                name: 'Module1'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1  {
+        color: red;
+    }
+</style>
---
+++ public/js/src/components/Module2.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{name}}</h1>
+</template>
+
+<script>
+    export default {
+        name: "Module2",
+        data() {
+            return {
+                name: 'Module2'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1  {
+        color: pink;
+    }
+</style>
