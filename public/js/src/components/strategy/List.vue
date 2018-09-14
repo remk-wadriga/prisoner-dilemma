392c991a118d0b27ecf84e8698622e5a9a8a2436
Rem <dkushneriov@aurum-soft.com>
2018-09-15 00:34:13 +0300

    Frontend:    Filtering, sorting and pagination of strategies list

--- public/js/src/components/strategy/List.vue
+++ public/js/src/components/strategy/List.vue
@@ -10,11 +10,31 @@
         data() {
             return {
                 strategies: [],
-                fields: ['name', 'description', 'status', 'actions'],
+                fields: [
+                    {
+                        key: 'name',
+                        sortable: true
+                    },
+                    {
+                        key: 'status',
+                        sortable: true
+                    },
+                    {
+                        key: 'shortDescription',
+                        label: 'Description',
+                        sortable: false
+                    },
+                    'actions',
+                ],
                 deleteStrategyVisible: false,
                 viewBtnVar: 'primary',
                 updateBtnVar: 'success',
                 deleteBtnVar: 'danger',
+                filter: '',
+                status: '',
+                currentPage: 1,
+                perPage: 10,
+                totalRows: 0,
             }
         },
         methods: {
@@ -34,10 +54,19 @@
                 })
                 this.$store.commit('selectedStrategy', strategy)
                 this.deleteStrategyVisible = true
+            },
+            onFiltered(filteredItems) {
+                // Trigger pagination to update the number of buttons/pages due to filtering
+                this.totalRows = filteredItems.length
+                this.currentPage = 1
             }
         },
         mounted() {
             Api.methods.request('app_homepage', {}, 'GET', response => {
+                response.forEach(item => {
+                    item.shortDescription = item.description.substring(0, 150) + '...'
+                    this.totalRows++
+                })
                 this.strategies = response
                 this.$store.commit('setPageTitle', '')
                 this.$store.commit('setContentTitle', 'Strategies')
