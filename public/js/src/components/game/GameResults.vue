49c84cba58bdf692cc5a84c3c3338ee3e0c496b6
Rem <dkushneriov@aurum-soft.com>
2018-10-26 03:39:16 +0300

    Frontend:    Game page ready simple results table ready

--- public/js/src/components/game/GameResults.vue
+++ public/js/src/components/game/GameResults.vue
@@ -5,7 +5,11 @@
         name: "GameResults",
         data() {
             return {
-
+                sum: 0,
+                score: [],
+                couples: [],
+                winner: null,
+                looser: null
             }
         },
         props: {
@@ -15,8 +19,48 @@
 
         },
         mounted() {
-            if (this.results != null) {
+            if (this.results != null && this.results.results !== undefined) {
+                if (this.results.results.sum !== undefined) {
+                    this.sum = this.results.results.sum
+                }
+                if (this.results.results.total !== undefined) {
+                    this.score = this.results.results.total
+                    this.score.sort((one, due) => { return one.result < due.result ? 1 : 0 })
+                }
+                if (this.results.results.couples !== undefined) {
+                    let tmpCoupleResIds = []
+                    this.score.forEach(res => {
+                        if (this.winner === null || this.winner.result < res.result) {
+                            this.winner = res
+                        }
+                        if (this.looser === null || this.looser.result > res.result) {
+                            this.looser = res
+                        }
 
+                        for (let index in this.results.results.couples) {
+                            if (!tmpCoupleResIds.includes(index)) {
+                                tmpCoupleResIds.push(index)
+
+                                let coupleRes = this.results.results.couples[index]
+                                let tmpCoupleRes = []
+
+                                index.split(':').forEach((id) => {
+                                    if (coupleRes[id] !== undefined) {
+                                        tmpCoupleRes.push({
+                                            id: id,
+                                            name: res.name,
+                                            result: coupleRes[id]
+                                        })
+                                    }
+                                })
+
+                                if (tmpCoupleRes.length === 2) {
+                                    this.couples.push(tmpCoupleRes)
+                                }
+                            }
+                        }
+                    })
+                }
             }
         }
     }
