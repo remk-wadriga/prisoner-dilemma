6b0898225f26dda3f9a327b7765df04b7eb83476
Rem <dkushneriov@aurum-soft.com>
2019-04-30 16:26:27 +0300

    Backend:    Remove data mapping from StrategyForm

--- src/Form/StrategyForm.php
+++ src/Form/StrategyForm.php
@@ -3,18 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Strategy;
-use Symfony\Component\Form\Exception\UnexpectedTypeException;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use App\Form\Type\ArrayType;
-use Symfony\Component\Form\DataMapperInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use App\Entity\Types\Enum\DecisionTypeEnum;
 
-class StrategyForm extends AbstractType implements DataMapperInterface
+class StrategyForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -62,43 +59,4 @@ class StrategyForm extends AbstractType implements DataMapperInterface
             'data_class' => Strategy::class,
         ]);
     }
-
-    public function mapDataToForms($data, $forms)
-    {
-        if (!$data instanceof Strategy) {
-            throw new UnexpectedTypeException($data, Strategy::class);
-        }
-
-        /** @var FormInterface[] $forms */
-        $forms = iterator_to_array($forms);
-
-        $forms['name']->setData($data->getName());
-        $forms['description']->setData($data->getDescription());
-        $forms['status']->setData($data->getStatus());
-        $decisionsData = $forms['decisionsData']->getData();
-        if (is_array($decisionsData)) {
-            $forms['decisionsData']->setData($decisionsData);
-        }
-    }
-
-    public function mapFormsToData($forms, &$data)
-    {
-        if (!$data instanceof Strategy) {
-            throw new UnexpectedTypeException($data, Strategy::class);
-        }
-
-        /** @var FormInterface[] $forms */
-        $forms = iterator_to_array($forms);
-
-        $data->setName($forms['name']->getData());
-        $data->setDescription($forms['description']->getData());
-        $data->setStatus($forms['status']->getData());
-        $decisionsData = $forms['decisionsData']->getData();
-        dd($decisionsData);
-        if (is_array($decisionsData)) {
-            $data->setDecisionsData($decisionsData);
-        }
-    }
-
-
 }
