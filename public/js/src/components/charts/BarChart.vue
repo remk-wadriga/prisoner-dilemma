de1b799f29fdeda9409dff393462c7d2dd3db777
Rem <dkushneriov@aurum-soft.com>
2019-05-06 18:31:15 +0300

    Frontend:    Add styles to "BarChart" component

--- public/js/src/components/charts/BarChart.vue
+++ public/js/src/components/charts/BarChart.vue
@@ -48,6 +48,18 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+    .chart-container {
+        background: #212733;
+        border-radius: 15px;
+        box-shadow: 0px 2px 15px rgba(25, 25, 25, 0.27);
+        margin:  25px 0;
+        width: 100%;
+    }
+    .chart-container h2 {
+        margin-top: 0;
+        padding: 15px 0;
+        color:  rgba(255, 0,0, 0.5);
+        border-bottom: 1px solid #323d54;
+    }
 </style>
